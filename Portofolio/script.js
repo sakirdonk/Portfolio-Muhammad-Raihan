dfafb06243391ca0a87a36d675fc95f07d3815a6
sakirdonk <40842577+sakirdonk@users.noreply.github.com>
2024-04-01 17:24:01 +0700

    Add files via upload

---
+++ Portofolio/script.js
@@ -0,0 +1,23 @@
+function react1(){
+    document.getElementById("project1").style.background="white";
+    document.getElementById("project1").style.color="black";
+    document.getElementById("icon1").src="https://cdn-icons-png.flaticon.com/512/32/32213.png";
+}
+
+function unreact1(){
+    document.getElementById("project1").style.background="black";
+    document.getElementById("project1").style.color="white";
+    document.getElementById("icon1").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy4SICM2Rfxy_dpQae-OfjS6b0aDxY2PUBTwCPl1OU0Ivy5Iy4r8tJqZVjrdIrpzWgpHs&usqp=CAU";
+}
+
+function react2(){
+    document.getElementById("project2").style.background="white";
+    document.getElementById("project2").style.color="black";
+    document.getElementById("icon2").src="https://cdn-icons-png.flaticon.com/512/32/32213.png";
+}
+
+function unreact2(){
+    document.getElementById("project2").style.background="black";
+    document.getElementById("project2").style.color="white";
+    document.getElementById("icon2").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy4SICM2Rfxy_dpQae-OfjS6b0aDxY2PUBTwCPl1OU0Ivy5Iy4r8tJqZVjrdIrpzWgpHs&usqp=CAU";
+}
